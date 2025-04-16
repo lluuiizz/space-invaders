bd26289b2276af3350532ecc5f2b664b7b4a4deb
Luiz Guilherme Silva <luiz.developer.acc@gmail.com>
2025-04-16 20:15:21 -0300

    Changed some function definitions

--- include/objects.h
+++ include/objects.h
@@ -6,13 +6,14 @@
 typedef enum {
 	Player,
 	Enemy,
+	Bullet,
 
 } Object_type;
+#define NUM_OF_DIFFERENT_OBJECTS 3
 
 typedef struct {
 	Object_type  obj_type;
 	SDL_Texture* sprite;
-	int          state;
 	float		 pos_x, pos_y;
 	SDL_Rect	 collision;	//dst_rect:  pos_x pos_y, width_height
 	
--- include/space_invaders.h
+++ include/space_invaders.h
@@ -6,6 +6,7 @@
 #include <stdbool.h>
 
 #include "objects.h"
+#include "bullet.h"
 
 #define WIDTH 640
 #define HEIGHT 480
@@ -13,7 +14,7 @@
 #define FRAMES 30
 #define TICKS_PER_FRAME 1000/FRAMES
 
-void render_game_objects (SDL_Renderer *render, Objects enemys, Object *player);
+void render_game_objects (SDL_Renderer *render, Bullet_list *bullet_list, Objects*);
 
 #endif
 
