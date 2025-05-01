8c6a5c1716710404f3a3cb5eb5a032290e9ab134
Luiz Guilherme Silva <luiz.developer.acc@gmail.com>
2025-04-30 22:29:11 -0300

    Adicionado BALAS a struct PLAYER

--- include/player.h
+++ include/player.h
@@ -13,6 +13,7 @@
 #define LEFT_MOVEMENT SDL_SCANCODE_A
 #define ATTACK		  SDLK_SPACE
 
+#define BULLET_PLAYER_COULDOWN 0.25
 
 void init_player_state(SDL_Renderer* render, game_state_t *);
 void handle_keyboard_input  (SDL_Renderer *render, game_state_t *);
@@ -24,6 +25,8 @@ struct player
 	object_t *render_info;
 	bool attacking;
 	int move_speed;
+
+	bullet_list_t *bullets;
 };
 
 #endif
